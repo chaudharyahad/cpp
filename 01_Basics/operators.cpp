9eb4055df708e39604481b4ff897fbb1053c21c4
Ahad Chaudhary <ahadchaudharyyy@gmail.com>
2026-09-01 12:03:43 +0500

    feat: add basic arithmetic operations demonstration

---
+++ 01_Basics/operators.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"==========================\n";
+    cout<<"Airthmetic Operator\n";
+    cout<<"==========================\n";
+
+    int a = 10, b = 3;
+    // Addition
+    cout << "a + b = " << (a + b) << endl;
+
+    // Subtraction
+    cout << "a - b = " << (a - b) << endl;
+
+    // Multiplication
+    cout << "a * b = " << (a * b) << endl;
+
+    // Division
+    cout << "a / b = " << (a / b) << endl;
+
+    // Modulos
+    cout << "a % b = " << (a % b) << endl;
+
+    // Increament
+    cout << "++a = " << ++a << endl;
+
+    // Decrement
+    cout << "b-- = " << b--;
+
+    return 0;
+}

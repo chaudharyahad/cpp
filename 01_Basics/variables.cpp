dfc75d37ca13894ffdd76f8005ac3814d210ce95
Ahad Chaudhary <ahadchaudharyyy@gmail.com>
2026-09-01 11:35:01 +0500

    feat: add variables demonstration program

---
+++ 01_Basics/variables.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // Declare and initialize an integer variable
+    int num = 10;
+
+    // Access the variable
+    cout << "Initial value: " << num << endl;
+
+    // Update the variable
+    num = 20;
+
+    // Print the updated value
+    cout << "Updated value: " << num << endl;
+
+    // Declare two integer variables
+    int num1 = 100;
+    int num2;
+
+    // Assign the value of num1 to num2
+    num2 = num1;
+
+    cout << "num1: " << num1 << endl;
+    cout << "num2: " << num2 << endl;
+
+    return 0;
+}

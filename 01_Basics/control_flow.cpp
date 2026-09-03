992b09ae4b1e066a13f24d20f54befc3d2b167ed
Ahad Chaudhary <ahadchaudharyyy@gmail.com>
2026-09-03 11:15:09 +0500

    feat: add if-else-if statement for additional condition checks

--- 01_Basics/control_flow.cpp
+++ 01_Basics/control_flow.cpp
@@ -17,4 +17,21 @@ int main()
     {
         cout << "a is not equal to 18" << endl;
     }
+
+    cout << "==========================\n";
+    cout << "if-else-if Statement\n";
+    cout << "==========================\n";
+
+    if (a == 20)
+    {
+        cout << "a is equal to 20" << endl;
+    }
+    else if (a == 10)
+    {
+        cout << "a is equal to 10" << endl;
+    }
+    else
+    {
+        cout << "a is not equal to 20 or 10";
+    }
 }

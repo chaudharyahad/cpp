fb017cff9715a2abaa23955300ace398fbf78e4b
Ahad Chaudhary <ahadchaudharyyy@gmail.com>
2026-09-03 11:14:16 +0500

    feat: implement if-else statement

---
+++ 01_Basics/control_flow.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int a = 10;
+
+    cout << "==========================\n";
+    cout << "if-else Statement\n";
+    cout << "==========================\n";
+
+    if (a == 18)
+    {
+        cout << "a is equal to 18" << endl;
+    }
+    else
+    {
+        cout << "a is not equal to 18" << endl;
+    }
+}

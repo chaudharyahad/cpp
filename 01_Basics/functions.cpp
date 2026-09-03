d3fc743d27280f8bcdd0eaac51555b3288d52d25
Ahad Chaudhary <ahadchaudharyyy@gmail.com>
2026-09-03 17:53:32 +0500

    style: format code for consistency and readability and add passByValue and passByRefrence function

--- 01_Basics/functions.cpp
+++ 01_Basics/functions.cpp
@@ -1,22 +1,62 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-// function definition
-void greet(){
-    cout<<"Welcome to C++ Programming"<<endl;
+// Function definition
+void greet()
+{
+    cout << "Welcome to C++ Programming" << endl;
 }
-int multiply(int x,int y){
+
+// Function with return value
+int multiply(int x, int y)
+{
     return x * y;
 }
+
+// Pass by value
+void passByValue(int a)
+{
+    a = 20;
+}
+
+// Pass by reference
+void passByReference(int &b)
+{
+    b = 50;
+}
+
 int main()
 {
-    int num=5,result;
+    int num = 5;
+    int result;
 
     // Calling the function
     greet();
-    result=multiply(num,10);
 
-    cout<<"Multiplication result : "<<result<<endl;
+    // Calling multiply function
+    result = multiply(num, 10);
+
+    cout << "Multiplication result: " << result << endl;
+
+    cout << "======================" << endl;
+    cout << "Pass by Value" << endl;
+    cout << "======================" << endl;
+
+    int a = 10;
+
+    passByValue(a);
+
+    cout << "a: " << a << endl;
+
+    cout << "======================" << endl;
+    cout << "Pass by Reference" << endl;
+    cout << "======================" << endl;
+
+    int b = 20;
+
+    passByReference(b);
+
+    cout << "b: " << b << endl;
+
     return 0;
-    
 }

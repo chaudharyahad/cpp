f61d0a34c60557dbc6dc0e0e4e6fbbb00ea7c953
Ahad Chaudhary <ahadchaudharyyy@gmail.com>
2026-09-16 16:59:11 +0500

    feat: add integer data type example

---
+++ 01_Basics/data_types.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // Integer variable
+    int number = 10;
+    cout << "Integer: " << number << endl;
+    cout << "Size: " << sizeof(number) << " bytes" << endl;
+
+    return 0;
+}

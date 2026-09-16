f9eb0d04f070b3467f3524c35f24d7a13da51473
Ahad Chaudhary <ahadchaudharyyy@gmail.com>
2026-09-16 17:00:05 +0500

    feat: add character data type example

--- 01_Basics/data_types.cpp
+++ 01_Basics/data_types.cpp
@@ -8,6 +8,12 @@ int main()
     int number = 10;
     cout << "Integer: " << number << endl;
     cout << "Size: " << sizeof(number) << " bytes" << endl;
+    
+    cout << "=======================" << endl;
+    // Character variable
+    char character = 'A';
+    cout << "Character: " << character << endl;
+    cout << "Size: " << sizeof(character) << " byte" << endl;
 
     return 0;
 }

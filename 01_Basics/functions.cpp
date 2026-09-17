c80dbfcd43528ed0bf031da77aee5f02f58c46ee
Ahad Chaudhary <ahadchaudharyyy@gmail.com>
2026-09-17 22:37:46 +0500

    feat: add greet function example

---
+++ 01_Basics/functions.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void greet(); // Function declaration
+
+int main()
+{
+    // Calling the function
+    greet();
+
+    return 0;
+}
+
+// Function definition
+void greet()
+{
+    cout << "Welcome to C++ Programming" << endl;
+}

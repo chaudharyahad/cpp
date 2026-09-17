fb66bb2007620427e2c6b5c0751305ce5bb2d7dc
Ahad Chaudhary <ahadchaudharyyy@gmail.com>
2026-09-17 15:46:01 +0500

    feat: add relational operators

--- 01_Basics/operators.cpp
+++ 01_Basics/operators.cpp
@@ -29,5 +29,26 @@ int main()
     // Decrement
     cout << "b-- = " << b--;
 
+    cout << "\n==========================\n";
+    cout << "Relational Operator\n";
+    cout << "==========================\n";
+
+    // Less than Operator
+    cout << "a < b = " << (a < b) << endl;
+
+    // Greater than Operator
+    cout << "a > b = " << (a > b) << endl;
+
+    // Less than Equal to Operator
+    cout << "a <= b = " << (a <= b) << endl;
+
+    // Greater than Equal to Operator
+    cout << "a >= b = " << (a >= b) << endl;
+
+    // Equal to Operator
+    cout << "a == b = " << (a == b) << endl;
+
+    // Not Equal to Operator
+    cout << "a != b = " << (a != b) << endl;
     return 0;
 }

55ab34712f59855dd3d3990409c26cf3a1971dc0
Ahad Chaudhary <ahadchaudharyyy@gmail.com>
2026-09-17 22:04:42 +0500

    feat: implement switch case statement

--- 01_Basics/controlflow.cpp
+++ 01_Basics/controlflow.cpp
@@ -31,7 +31,7 @@ int main()
     }
     else
     {
-        cout << "a is not equal to 20 or 10"<<endl;
+        cout << "a is not equal to 20 or 10" << endl;
     }
 
     cout << "==========================\n";
@@ -39,6 +39,22 @@ int main()
     cout << "==========================\n";
 
     cout << ((a == 10) ? "a is equal to 10" : "a is not equal to 10") << endl;
+
+    cout << "==========================\n";
+    cout << "Switch-Case Statement\n";
+    cout << "==========================\n";
+
+    switch (a)
+    {
+    case 5:
+        cout << "a is equal to 5" << endl;
+        break;
+    case 10:
+        cout << "a is equal to 10" << endl;
+        break;
+    default:
+        cout << "a is not equal to 5 or 10" << endl;
+    }
 
     return 0;
 }

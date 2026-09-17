aa2604426adfc060b688f7c6488d3d0559902806
Ahad Chaudhary <ahadchaudharyyy@gmail.com>
2026-09-17 22:51:37 +0500

    feat: implement multiply function with return type

--- 01_Basics/functions.cpp
+++ 01_Basics/functions.cpp
@@ -3,10 +3,22 @@ using namespace std;
 
 void greet(); // Function declaration
 
+// Function with return value
+int multiply(int x, int y)
+{
+    return x * y;
+}
+
 int main()
 {
+    int result, num = 5;
     // Calling the function
     greet();
+
+    // Calling multiply function
+    result = multiply(num, 10);
+
+    cout << "Multiplication result: " << result << endl;
 
     return 0;
 }

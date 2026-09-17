64d1bb2acdd85513baf204a58cf29a779c25c68f
Ahad Chaudhary <ahadchaudharyyy@gmail.com>
2026-09-17 15:43:51 +0500

    feat: add airthmetic operators

---
+++ 01_Basics/operators.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 10, b = 3;
+
+    cout << "==========================\n";
+    cout << "Airthmetic Operator\n";
+    cout << "==========================\n";
+
+    // Addition
+    cout << "a + b = " << (a + b) << endl;
+
+    // Subtraction
+    cout << "a - b = " << (a - b) << endl;
+
+    // Multiplication
+    cout << "a * b = " << (a * b) << endl;
+
+    // Division
+    cout << "a / b = " << (a / b) << endl;
+
+    // Modulos
+    cout << "a % b = " << (a % b) << endl;
+
+    // Increament
+    cout << "++a = " << ++a << endl;
+
+    // Decrement
+    cout << "b-- = " << b--;
+
+    return 0;
+}

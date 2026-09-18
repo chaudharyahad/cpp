52351eaaa7e711a4928310b0e1e97480e0f7dbee
Ahad Chaudhary <ahadchaudharyyy@gmail.com>
2026-09-18 21:42:09 +0500

    feat: add size and length calculations for array

--- 01_Basics/array.cpp
+++ 01_Basics/array.cpp
@@ -23,5 +23,15 @@ int main()
 
     cout << endl;
 
+    // Size of one element of an array
+    cout << "Size of arr[0]: " << sizeof(arr[0]) << endl;
+
+    // Size of  'arr'
+    cout << "Size of arr: " << sizeof(arr) << endl;
+
+    // Length of an array
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << "Length of an array: " << n << endl;
+
     return 0;
 }
